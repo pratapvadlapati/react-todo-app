2233df9ce3ceb2cdfba549bb85d3c59be259dc11
pratap <pratapvadlapati@gmail.com>
2020-06-03 19:39:32 +0400

    Updates

---
+++ precache-manifest.dda5ebb099a40aa01ee4b8a68223a666.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ca13986a1f5268ba8faaa4f1350397",
+    "url": "/pratapvadlapati/react-todo-app/index.html"
+  },
+  {
+    "revision": "1e6dae956efdff6281a5",
+    "url": "/pratapvadlapati/react-todo-app/static/css/2.9b8e7531.chunk.css"
+  },
+  {
+    "revision": "a237951d58c3359fe1ff",
+    "url": "/pratapvadlapati/react-todo-app/static/css/main.ace743e4.chunk.css"
+  },
+  {
+    "revision": "1e6dae956efdff6281a5",
+    "url": "/pratapvadlapati/react-todo-app/static/js/2.2b8522f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pratapvadlapati/react-todo-app/static/js/2.2b8522f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a237951d58c3359fe1ff",
+    "url": "/pratapvadlapati/react-todo-app/static/js/main.824bd45e.chunk.js"
+  },
+  {
+    "revision": "d6122608f752dcaf0711",
+    "url": "/pratapvadlapati/react-todo-app/static/js/runtime-main.b53e1453.js"
+  }
+]);
